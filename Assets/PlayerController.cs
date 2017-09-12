be337852da0811b98c8a617dcd2c3ee2826c5297
Lucas <lg.piergiacomi@gmail.com>
2017-09-12 15:33:28 -0300

    Sprites. Estados. Movimiento horizontal

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float maxSpeed = 5f;
+    public float speed = 2f;
+
+    private Rigidbody2D myrb2d;
+
+	// Use this for initialization
+	void Start () {
+        myrb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        float direccion = Input.GetAxis("Horizontal");
+
+        myrb2d.AddForce(Vector2.right * speed * direccion);
+
+        if (myrb2d.velocity.x > maxSpeed)
+        {
+            myrb2d.velocity = new Vector2(maxSpeed, myrb2d.velocity.y);
+        }
+        if (myrb2d.velocity.x < -maxSpeed)
+        {
+            myrb2d.velocity = new Vector2(-maxSpeed, myrb2d.velocity.y);
+        }
+    }
+}
