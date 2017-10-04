db120da9623bf511644707ffbd4a342e5da4848d
Lucas <lg.piergiacomi@gmail.com>
2017-10-03 22:52:29 -0300

    Añadido scripts faltantes

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour {
+
+    private Animator enemyAnim;
+    public float animDelay;
+
+    public int enemyHealth;
+    public int enemyValue;
+
+    public static bool enemyDead;
+
+    void Start()
+    {
+        enemyAnim = GetComponent<Animator>();
+    }
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Bullet")
+        {
+            enemyHealth -= BulletMovement.damage;
+            if (enemyHealth <= 0)
+            {
+                enemyDead = true;
+                enemyAnim.SetBool("isDead", true);
+                Debug.Log(enemyValue);
+                Destroy(gameObject, animDelay);
+            }
+        }
+    }
+}
