c7f2cef7f0318e9d3d022c408c5d859d14cc17e0
Ramiro Belluardo <ramirobelluardo1993@gmail.com>
2017-10-04 09:57:53 -0300

    Mejorado daño del enemigo al personaje

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -14,6 +14,8 @@ public class HealthManager : MonoBehaviour {
 
     public static bool playerDead;
 
+	public bool invincible;
+
     // Use this for initialization
     void Start () {
         playerDead = false;
@@ -22,28 +24,34 @@ public class HealthManager : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        if(col.tag == "Enemigo")
-        {
-            playerHealth -= enemyDamage;
-            if(playerHealth > 0)
-            {
-                GetComponent<SpriteRenderer>().color = Color.red;
-                if (col.GetComponent<SpriteRenderer>().flipX == false)
-                {
-                    myRigidbody.velocity = new Vector2(-KnockBackX, KnockBackY);
-                }
-                else
-                {
-                myRigidbody.velocity = new Vector2(KnockBackX, KnockBackY);
-                }
-            }
-            else
-            {
-                playerDead = true;
-                GetComponent<SpriteRenderer>().enabled = false;
-                GetComponent<BoxCollider2D>().enabled = false;
-            }
-        }
+		if (!invincible) 
+		{
+			if(col.tag == "Enemigo")
+			{
+				playerHealth -= enemyDamage;
+				if(playerHealth > 0)
+				{
+					GetComponent<SpriteRenderer>().color = Color.red;
+					invincible = true;
+					StartCoroutine ("tiempoEspera");
+					if (col.GetComponent<SpriteRenderer>().flipX == false)
+					{
+						myRigidbody.velocity = new Vector2(-KnockBackX, KnockBackY);
+					}
+					else
+					{
+						myRigidbody.velocity = new Vector2(KnockBackX, KnockBackY);
+					}
+				}
+				else
+				{
+					playerDead = true;
+					GetComponent<SpriteRenderer>().enabled = false;
+					GetComponent<BoxCollider2D>().enabled = false;
+				}
+			}
+		}
+
     }
 
     void OnTriggerExit2D(Collider2D col)
@@ -53,4 +61,12 @@ public class HealthManager : MonoBehaviour {
             GetComponent<SpriteRenderer>().color = Color.white;
         }
     }
+
+	IEnumerator tiempoEspera() {
+		yield return new WaitForSeconds (1.2f);
+		invincible = false;
+
+	}
+
 }
+
