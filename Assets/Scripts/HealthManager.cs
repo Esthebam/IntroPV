e04d7c29bddd96c6f8ff7a3d65af3fc64a114ed8
Lucas <lg.piergiacomi@gmail.com>
2017-10-04 00:43:21 -0300

    Implementado Knockback

---
+++ Assets/Scripts/HealthManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour {
+
+    Rigidbody2D myRigidbody;
+
+    public float KnockBackX;
+    public float KnockBackY;
+
+    // Use this for initialization
+    void Start () {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Enemigo")
+        {
+            if (col.GetComponent<SpriteRenderer>().flipX == false)
+            {
+            myRigidbody.velocity = new Vector2(-KnockBackX, KnockBackY);
+            }
+            else
+            {
+            myRigidbody.velocity = new Vector2(KnockBackX, KnockBackY);
+            }
+        }
+    }
+
+}
