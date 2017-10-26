cc6bf3ea2c668674852db9fd8410a5569792dcf3
Lucas <lg.piergiacomi@gmail.com>
2017-10-26 13:10:48 -0300

    Solucionados powerups!

---
+++ Assets/Scripts/ChequearPisoPowerUp.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChequearPisoPowerUp : MonoBehaviour {
+
+    private Rigidbody2D myRigidbody;
+
+    // Use this for initialization
+    void Start () {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Piso")
+        {
+            myRigidbody.bodyType = RigidbodyType2D.Static;
+        }
+    }
+
+}
---
+++ Assets/Scripts/LootDrop.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootDrop : MonoBehaviour {
+
+    public bool tocandoPiso;
+
+    public GameObject powerUpVidaPrefab;
+	public GameObject powerUpSaltoPrefab;
+	public GameObject powerUpDmgPrefab;
+	public GameObject powerUpVelocidadPrefab;
+	public LayerMask escenario;
+	// Use this for initialization
+	void Start () {
+    }
+
+
+
+
+    void SpawnPowerUpVida () {
+		GameObject powerUpVida = Instantiate(powerUpVidaPrefab);
+		powerUpVida.transform.position = transform.position;
+		Rigidbody2D rBody = powerUpVida.GetComponent<Rigidbody2D>();
+		rBody.AddForce(new Vector2(Random.Range(3,-3), 7), ForceMode2D.Impulse);
+
+    }
+	void SpawnPowerUpSalto () {
+		GameObject powerUpSalto = Instantiate(powerUpSaltoPrefab);
+		powerUpSalto.transform.position = transform.position;
+		Rigidbody2D rBody = powerUpSalto.GetComponent<Rigidbody2D>();
+		rBody.AddForce(new Vector2(Random.Range(3,-3), 7), ForceMode2D.Impulse);
+
+	}
+	void SpawnPowerUpDmg () {
+		GameObject powerUpDmg = Instantiate(powerUpDmgPrefab);
+
+		powerUpDmg.transform.position = transform.position;
+		Rigidbody2D rBody = powerUpDmg.GetComponent<Rigidbody2D>();
+		rBody.AddForce(new Vector2(Random.Range(3,-3), 7), ForceMode2D.Impulse);
+
+	}
+	void SpawnPowerUpVelocidad () {
+		GameObject powerUpVelocidad = Instantiate(powerUpVelocidadPrefab);
+		powerUpVelocidad.transform.position = transform.position;
+		Rigidbody2D rBody = powerUpVelocidad.GetComponent<Rigidbody2D>();
+		rBody.AddForce(new Vector2(Random.Range(3,-3), 7), ForceMode2D.Impulse);
+
+
+	}
+		
+
+	public void DropLot(){
+		SpawnPowerUpVida ();
+		SpawnPowerUpVelocidad ();
+		SpawnPowerUpDmg ();
+		SpawnPowerUpSalto ();
+	}
+}
