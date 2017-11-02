0ef1ad1bd7041e8f9ca55c7f98c41c36d0204a42
Esteban Matas <estebanmatas13@gmail.com>
2017-11-02 01:01:07 -0300

    Plataformas moviles andando

--- Assets/Scripts/ChequearPiso.cs
+++ Assets/Scripts/ChequearPiso.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 public class ChequearPiso : MonoBehaviour {
 
     private PlayerController player;
+	private Rigidbody2D myRigidbody;
 
 	// Use this for initialization
 	void Start () {
         player = GetComponentInParent<PlayerController>();
+		myRigidbody = GetComponentInParent<Rigidbody2D>();
+	}
+
+	private void OnCollisionEnter2D(Collision2D col){
+		if(col.gameObject.tag == "PlataformaMovil")
+		{
+			myRigidbody.velocity = new Vector3(0f,0f,0f);
+			player.transform.parent = col.transform;
+			player.tocandoPiso = true;
+		}    
 	}
 
     private void OnCollisionStay2D(Collision2D col)
@@ -17,8 +28,11 @@ public class ChequearPiso : MonoBehaviour {
         {
             player.tocandoPiso = true;
         }
-
-        
+		if(col.gameObject.tag == "PlataformaMovil")
+		{
+			player.transform.parent = col.transform;
+			player.tocandoPiso = true;
+		}      
     }
 
     private void OnCollisionExit2D(Collision2D col)
@@ -27,7 +41,11 @@ public class ChequearPiso : MonoBehaviour {
         {
 			player.tocandoPiso = false;
 		}
-        
+		if(col.gameObject.tag == "PlataformaMovil")
+		{
+			player.transform.parent = null;
+			player.tocandoPiso = false;
+		}
     }
 
 
